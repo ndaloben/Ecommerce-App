db2b26109e04751f674110a62932881f52c3d135
ben <ndaloben8@gmail.com>
2023-08-02 14:26:01 +0300

    product updates

--- app/(.)product/[id]/page.tsx
+++ app/(.)product/[id]/page.tsx
@@ -8,6 +8,18 @@ import { useEffect, useState } from "react";
 
 function Modal() {
     const [isOpen, setIsOpen] = useState(true);
+    const id  = useParams().id;
+    const [product, setProduct] = useState<Product>();
+
+    useEffect(() => {
+        async function fetchProduct() {
+            const res = await fetch(`https://fakestoreapi.com/products ${id}`);
+            const product = await res.json();
+            console.log(product);
+
+        }
+
+    }, [id]);
 
     return (
         <Dialog
@@ -21,11 +33,23 @@ function Modal() {
         {/* Full-screen container to center the panel */}
         <div className="fixed inset-0 flex items-center justify-center p-4">
           {/* The actual dialog panel  */}
-          <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
-            <Dialog.Title>Complete your order</Dialog.Title>
+          <Dialog.Panel className="mx-auto max-w-sm rounded bg-white p-10">
+            
+                <div className="flex gap-x-8 h-96">
+                    {product?.image && (
+                        <div className="relative w-72 h-full">
+                      <ProductImage product={product} fill={true} />  
+                      </div>
+                    )}
+                    <div>
+                        <div>
+                            <h1 className="font-medium">{product?.title}</h1>
+                            <p className='font-medium text-sm'>${product?.price}</p>
+                        </div>
+                    </div>
+                </div>
   
-            {/* ... */}
-          </Dialog.Panel>
+           </Dialog.Panel>
         </div>
       </Dialog>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-8 mt-48">
      <section className='flex flex-col space-y-12 pb-44'>
-     <h1 className="text-5xl font-bold text-center">HOT DEALS</h1>
+     <h1 className="text-5xl font-bold text-center">🔥HOT DEALS</h1>
      <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
       {products.map((product) => (
          <Product key={product.id} product={product}/>
