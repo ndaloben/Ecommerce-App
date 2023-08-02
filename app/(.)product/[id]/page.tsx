6e5e53e33590750a1034b46af6808fbcce16bdaa
ben <ndaloben8@gmail.com>
2023-08-02 12:39:35 +0300

    updates

--- app/(.)product/[id]/page.tsx
+++ app/(.)product/[id]/page.tsx
@@ -1,2 +1,23 @@
+"use client";
+
 import ProductImage from "@/app/components/ProductImage";
 import { Dialog } from "@headlessui/react";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+
+function Modal() {
+    const [isOpen, setIsOpen] = useState(true);
+
+    return (
+        <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className='relative z-50'
+        >
+            <div className="fixed inset-0 bg-black/30" aria-hidden="true">
+                
+            </div>
+        </Dialog>
+    );
+}
