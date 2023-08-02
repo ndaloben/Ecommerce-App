d03a82b1700026db3996f23f4067b1e9cb89ed34
ben <ndaloben8@gmail.com>
2023-08-02 13:17:15 +0300

    next version update

--- app/(.)product/[id]/page.tsx
+++ app/(.)product/[id]/page.tsx
@@ -13,11 +13,22 @@ function Modal() {
         <Dialog
         open={isOpen}
         onClose={() => setIsOpen(false)}
-        className='relative z-50'
-        >
-            <div className="fixed inset-0 bg-black/30" aria-hidden="true">
-                
-            </div>
-        </Dialog>
+        className="relative z-50"
+      >
+        {/* The backdrop, rendered as a fixed sibling to the panel container */}
+        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+  
+        {/* Full-screen container to center the panel */}
+        <div className="fixed inset-0 flex items-center justify-center p-4">
+          {/* The actual dialog panel  */}
+          <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
+            <Dialog.Title>Complete your order</Dialog.Title>
+  
+            {/* ... */}
+          </Dialog.Panel>
+        </div>
+      </Dialog>
     );
 }
+
+export default Modal
